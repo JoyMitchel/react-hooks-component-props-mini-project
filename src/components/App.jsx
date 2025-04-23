395b8c97dfc3998b468f54e23b1d02d250bc0133
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-04-23 13:29:12 +0300

    completed assignment

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+
+
+console.log(blogData);
+
+function App() {
+  const blogName = blogData.name;
+  const blogImage = blogData.image;
+  const blogAbout = blogData.about;
+  const blogPosts = blogData.posts;
+
+  return (
+    <div className="App">
+      <Header name={blogName} />
+      <About image={blogImage} about={blogAbout} />
+      <ArticleList posts={blogPosts} />
+    </div>
+  );
+}
+
+export default App;
